861b1fdab6f8b6d32e9fcfe2330c0404c6563370
Shoko <a.breyton.pro@gmail.com>
2026-02-09 11:39:31 +0100

    feat(core): construct row-major Matrix abstraction over 1D Vector

---
+++ Matrix.hpp
@@ -0,0 +1,23 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include "Vector.hpp"
+
+/**
+ * Basic header of matrices
+ * Currently in row major, but i might implement column-major later.
+ * Created the 15/02/2026 by Shoko_ofi
+ */
+
+struct Matrix {
+  int m;
+  int n;
+
+  Vector data;
+  Matrix(int rows, int cols);
+
+  double &operator()(int i, int j);
+  const double &operator()(int i, int j) const;
+};
+
+#endif // MATRIX_HPP
