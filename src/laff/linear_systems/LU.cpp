8732aa831549be2807e90ef6fed3a1397c459bab
Shoko-official <shoko.official.pro@gmail.com>
2026-03-06 11:48:58 +0100

    refactor: modularize LAFF routines into specialized source files
    Signed-off-by: Shoko-official <a.breyton.pro@gmail.com>

---
+++ src/laff/linear_systems/LU.cpp
@@ -0,0 +1,25 @@
+#include "laff/Laff.hpp"
+
+namespace laff {
+    bool lu_unb(Matrix& A) {
+        if (A.m != A.n) return false;
+        int n = A.m;
+        for (int k = 0; k < n; k++) {
+            double alpha_kk = A(k, k);
+            if (alpha_kk == 0.0) return false;
+            for (int i = k + 1; i < n; i++) A(i, k) /= alpha_kk;
+            for (int j = k + 1; j < n; j++) {
+                for (int i = k + 1; i < n; i++) {
+                    A(i, j) -= A(i, k) * A(k, j);
+                }
+            }
+        }
+        return true;
+    }
+
+    bool solve_lu(Matrix& A, Matrix& b) {
+        if (!trsv_ln(A, b)) return false;
+        if (!trsv_un(A, b)) return false;
+        return true;
+    }
+}
