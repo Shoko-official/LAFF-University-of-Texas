e028d93cd03d6d06741d6b3b0cfb68569e308a87
Shoko <a.breyton.pro@gmail.com>
2026-03-06 15:04:40 +0100

    feat(laff): implemented dot product and two-norm

--- Laff.cpp
+++ Laff.cpp
@@ -1,4 +1,5 @@
 #include "Laff.hpp"
+#include "Utility/Maths.hpp"
 
 /*
 * Implemented by Shoko on 2026-03-06
@@ -56,6 +57,35 @@ namespace laff {
             // y := alpha * x + y
             y.data[i] = alpha * x.data[i] + y.data[i];
         }
+        return true;
+    }
+
+    bool dot(const Matrix& x, const Matrix& y, double& alpha){
+        if ((x.m != 1 && x.n != 1) || (y.m != 1 && y.n != 1)) {
+            return false;
+        }
+        if (x.m * x.n != y.m * y.n) {
+            return false;
+        }
+        int size = x.m * x.n;
+        alpha = 0.0;
+        
+        for (int i = 0; i < size; i++){
+            alpha += x.data[i] * y.data[i];
+        }
+        return true;
+    }
+
+    bool norm2(const Matrix& x, double& alpha) {
+        if (x.m != 1 && x.n != 1) {
+            return false;
+        }
+
+        if (!dot(x, x, alpha)) {
+            return false;
+        }
+
+        alpha = Maths::sqrt(alpha);
         return true;
     }
 }
