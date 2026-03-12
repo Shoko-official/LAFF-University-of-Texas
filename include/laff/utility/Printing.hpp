89c8d89d9c10e75f6ce4634101b5cea1850e111e
Shoko <a.breyton.pro@gmail.com>
2026-03-12 15:50:12 +0100

    Feature: Add printing utility for matrices and vectors

---
+++ include/laff/utility/Printing.hpp
@@ -0,0 +1,14 @@
+#ifndef PRINTING_HPP
+#define PRINTING_HPP
+
+#include "../Matrix.hpp"
+
+namespace laff {
+    /**
+     * @brief Prints the matrix A to standard output.
+     * @param A Matrix to print
+     */
+    void print_matrix(const Matrix& A);
+}
+
+#endif // PRINTING_HPP
