3aa2778816d039561d5f25e87fef8ae446fdf5c4
Shoko-official <shoko.official.pro@gmail.com>
2026-06-05 22:52:30 +0200

    refactor(vector): convert Vector to template class and move implementation to header

--- include/laff/Vector.hpp
+++ include/laff/Vector.hpp
@@ -3,29 +3,73 @@
 
 /**
  * Basic header of vectors
- * Created the 2026/02/15 by Shoko_ofi
- * Editted the 2026/03/06 by Shoko_ofi
  */
 
+namespace laff {
+
+template <typename T = double>
 struct Vector {
-    double* data;
+    T* data;
     int length;
     int capacity;
     int count;
     bool owns_memory;
 
-    Vector(int size = 0, double val = 0.0);
-    Vector(double* prt, int size, int stride);
-    ~Vector();
+    Vector(int size = 0, T val = T(0)) : length(size), capacity(size > 0 ? size : 4), count(1), owns_memory(true) {
+        data = new T[capacity];
+        for (int i = 0; i < length; i++) {
+            data[i] = val;
+        }
+    }
 
-    Vector(const Vector& other);
-    Vector& operator=(const Vector& other);
+    Vector(T* ptr, int size, int stride) : data(ptr), length(size), capacity(size), count(stride), owns_memory(false) {}
 
-    double& operator[](int index);
+    ~Vector() {
+        if (owns_memory) {
+            delete[] data;
+        }
+    }
 
-    const double& operator[](int index) const;
+    Vector(const Vector& other) : length(other.length), capacity(other.length), count(1), owns_memory(true) {
+        data = new T[capacity];
+        for (int i = 0; i < length; ++i) {
+            data[i] = other.data[i * other.count];
+        }
+    }
 
-    Vector slice(int start, int end);
+    Vector& operator=(const Vector& other) {
+        if (this != &other) {
+            if (owns_memory) {
+                delete[] data;
+            }
+            length = other.length;
+            capacity = other.capacity;
+            count = 1;
+            owns_memory = true;
+            data = new T[capacity];
+            for (int i = 0; i < length; ++i) {
+                data[i] = other.data[i * other.count];
+            }
+        }
+        return *this;
+    }
+
+    T& operator[](int index) {
+        return data[index * count];
+    }
+
+    const T& operator[](int index) const {
+        return data[index * count];
+    }
+
+    Vector slice(int start, int end) {
+        return Vector(data + (start * count), end - start, count);
+    }
 };
+
+} // namespace laff
+
+// For compatibility with global namespace:
+using Vector = laff::Vector<double>;
 
 #endif // VECTOR_HPP
